83d9bb46e05fad6e021342a3e100535dd801ae13
alfmagic <alfdiaz91@gmail.com>
2025-06-14 15:37:11 -0500

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 export default {
   plugins: {
-    // This is the key change. We explicitly use the new package.
-    '@tailwindcss/postcss': {},
+    tailwindcss: {},
     autoprefixer: {},
   },
 }
