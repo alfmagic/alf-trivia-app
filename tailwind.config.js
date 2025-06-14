8638016cf19602ab2c83ab8d3da3eab284db1dcf
alfmagic <alfdiaz91@gmail.com>
2025-06-14 13:42:11 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./index.html", // <-- Make sure it scans your main HTML file
+    "./src/**/*.{js,ts,jsx,tsx}", // <-- Make sure it scans ALL files in your src folder
   ],
   theme: {
     extend: {},
